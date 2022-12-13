66b2632bb36062fb6cee4bc9eea7746f43e44538
Matthew Sun <mattsun220@gmail.com>
2022-12-13 16:00:11 -0500

    App registration + Migrations

--- builder/settings.py
+++ builder/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "resumes.apps.ResumesConfig",
+    "accounts.apps.AccountsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
