7d7eb7662aeebc79ff53e85f911e84bb2e18fb8f
Matthew Sun <mattsun220@gmail.com>
2022-12-13 17:17:20 -0500

    accounts views, forms, urls, and resumes home, create_resume, urls

--- builder/urls.py
+++ builder/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
+from resumes.views import home
+
+
+def redirect_to_resumes_list(request):
+    return redirect(home)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("resumes/", include("resumes.urls")),
+    path("", redirect_to_resumes_list, name="home_page"),
+    path("accounts/", include("accounts.urls")),
 ]
--- resumes/views.py
+++ resumes/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from resumes.models import *
+from django.contrib.auth.decorators import login_required
+from resumes.forms import *
 
-# Create your views here.
+
+@login_required
+def home(request):
+    resume_list = Resume.objects.filter(author=request.user)
+    context = {"resume_list": resume_list}
+    return render(request, "resumes/home.html", context)
+
+
+@login_required
+def create_resume(request):
+    if request.method == "POST":
+        form = ResumeForm(request.POST)
+        if form.is_valid():
+            resume = form.save(False)
+            resume.purchaser = request.user
+            resume.save()
+            return redirect("home")
+    else:
+        form = ResumeForm()
+    context = {"form": form}
+    return render(request, "resumes/create_resume.html", context)
